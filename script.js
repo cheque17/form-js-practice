354d81732931153b51f6938676187248a5342d17
cheque17 <ezequielperezrojas1999@users.noreply.github.com>
2023-10-31 11:18:11 -0600

    Edit styles.css and script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hi')
+console.log('Hi');
+
+const email = document.querySelector('#user-email');
+const country = document.querySelector('#user-country');
+const zipcode = document.querySelector('#user-zipcode');
+const password = document.querySelector('#password');
+const confirmationPw = document.querySelector('#password-confirmation');
+
+console.log(email)
